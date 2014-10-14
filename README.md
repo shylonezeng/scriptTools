d666771c5b775f7d0f7c1fad27dbffc48c89b8e8
zengxx_lab_ubuntu <shylonezeng@gmail.com>
2014-10-14 17:31:18 +0800

    finish basic function and renew README file

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-my bash tools Developed by bash shell script.
-all the tools are free under the term of General Public License
-enjoy them !
+## my bash tools Developed by bash shell script.
+## DESPRIPTION
+all the tools are free under the term of General Public License,you 
+can download them for free and modify them without asking for right.
+## SYNOPSIS
+toolName     				despription
+-------------------------------------------------------------------
+add_classprefix.sh [filename ...]	add prefix class name for class member functions in specfied  *.cpp files or other files
+					acp, short alias of add_classprefix.sh,should be set before using. you can also use other
+				       	alias name.
+next command
+
+## enjoy them !
+
--- add_classPrefix_forC++MemberFunction/add_classprefix.sh
+++ add_classPrefix_forC++MemberFunction/add_classprefix.sh
@@ -1,30 +1,44 @@
-# Last argument is  Classname
-# copy to origin file from temp file and romove temp one
-if  ;then
-fi
+## Last argument is  Classname
+## copy to origin file from temp file and romove temp one
 
-classname="Exampleclass"
-for arg in "$@" 
-do
-	files=$(find -name $arg)
-	if [ "$files" = "" ]; then
-		continue;
-        fi	
-	#else deal with files one by one;
-	for filename in $files 
+
+#search member function definition and add prefix class name
+
+function add_prefix { #begin function
+	
+	
+	#add class name for each member function in *.cpp  files
+	for arg in "$@" 
 	do
-		touch "$filename".temp
-		sed < $filename 's/\s*\(unsigned\|signed\)\?\(\s*void\|int\|char\|short\|long\|float\|double\)\(\(\s\|\*\)\+\)\([^):]*)\)$/\1\2\3'"$classname"'::\5/' > "$filename".temp
-	cat "$filename".temp	
-	done
-	#end for loop	
-done 
-#	echo $arg  
-# while do file in files
-# 	find functon definition line
-# 	locate add position
-# 	add prefix to files 
-# 	next file
-# end
-# 
-#test result:
+		files=$(find -name $arg)
+		if [ "$files" = "" ]; then
+			continue;
+	        fi	
+		#else deal with files one by one;
+		for filename in $files 
+		do
+			temp_filename="$filename".temp
+			touch $temp_filename
+			sed < $filename 's/\s*\(unsigned\|signed\)\?\(\s*void\|int\|char\|short\|long\|float\|double\)\(\(\s\|\*\)\+\)\([^):]*)\)$/\1\2\3'"$classname"'::\5/' > $temp_filename
+		#backup old file to undo delete and replace old file with renewed one	
+		backup_file=`dirname $filename`/.`basename $filename`.backup
+		echo $backup_file
+		mv $filename $backup_file
+		mv $temp_filename  $filename
+		done
+	
+		#end for loop	
+	done 
+} #function end
+
+
+#main entry
+classname="Exampleclass"
+echo -n "class name:"
+read  classname;
+
+while [ $classname = "" ] ; do 
+	read classname ;
+done
+add_prefix $@
+#end main
